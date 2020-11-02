28231f2b29128789973ef35fac5197624c166822
Kliffy <kliff.simon@gmail.com>
2020-11-02 13:46:29 -0800

    save

--- lib/sidekiq_mailer/worker.rb
+++ lib/sidekiq_mailer/worker.rb
@@ -2,6 +2,8 @@ class Sidekiq::Mailer::Worker
   include Sidekiq::Worker
 
   def perform(mailer_class, action, params)
+    byebug
+    1
     mailer_class.constantize.send(action, *params).deliver!
   end
 end
